369448b7ca0a8b9511e6899183eda3a3f48dd122
Srushti Sonavane <96761322+Srushti-S@users.noreply.github.com>
2023-05-26 18:23:57 +0530

    N-Queens Problem

--- LP2/A4.py
+++ LP2/A4.py
@@ -1,49 +1,50 @@
 def issafe(arr,x,y,n):
     for row in range(x):
         if arr[row][y] ==1:
-            return False        # Checking column attack
+            return False            # Checking column 
     row = x
     col = y
-    while row>=0 and col>=0:    #Checking Diagonal Attack
+     
+    while row>=0 and col>=0:        #Checking Diagonal 
         if arr[row][col]==1:
             return False
         row-=1
         col-=1
-
     row = x
     col = y
-    while row>=0 and col<n:     #Checking Anti Diagonal Attack
+    
+    while row>=0 and col<n:         #Checking Anti Diagonal 
         if arr[row][col]==1:
             return False
         row-=1
         col+=1
-
     return True
 
 def nQueen(arr,x,n):
-    if x>=n:
+    if x>=n:                        #Terminating condition
         return True
-
     for col in range(n):
         if issafe(arr,x,col,n):
             arr[x][col]=1
-            if nQueen(arr,x+1,n):
+            if nQueen(arr,x+1,n):   #checking terminating condition
                 return True
             arr[x][col] = 0
-
     return False
 
 def main():
     flag=1
-    while flag==1:
-        n = int(input("\nEnter number of Squares: "))
-        arr = [[0]*n for i in range(n)]
-        if nQueen(arr,0,n):
-            for i in range(n):
-                for j in range(n):
-                    print(arr[i][j],end=" ")
-                print()
-        flag=int(input("\nDo you want to continue(1/0)?: "))
+    while(flag==1):       
+        n = int(input("Enter number of squares : "))
+        if n<4:
+            print("Error! Please enter value greater than 3!!!")  
+        else:
+            arr = [[0]*n for i in range(0,n)]
+        
+            if nQueen(arr,0,n):
+                for i in range(n):
+                    for j in range(n):
+                        print(arr[i][j],end=" ")
+                    print()
+        flag=int(input("Do you want to continue(1/0)?: "))
 
-if __name__ == '__main__':
-    main()
+main()
